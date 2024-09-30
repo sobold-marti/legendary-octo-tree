48b839f96053d995d597379fdfdc358800a1241a
sobold-marti <martina@soboldltd.com>
2024-09-30 14:31:59 +0100

    feat: starter files

--- src/app/blog/[postId]/page.js
+++ src/app/blog/[postId]/page.js
@@ -1,17 +1,24 @@
-const page = async () => {
-	return (
-		<div className="single-blog-page">
-			<h2>Welcome to Lorem</h2>
-			<div className="blog-post">
-				<p>
-					Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur
-					ea dolore blanditiis. Eius natus incidunt doloribus exercitationem
-					dolorum, vero pariatur modi sapiente dignissimos iste quam facilis
-					aperiam. Ut, veritatis esse?
-				</p>
-			</div>
-		</div>
-	);
+async function getSinglePost(postId) {
+    const response = await fetch(
+        `${process.env.NEXT_PUBLIC_WORDPRESS_API_URL}/posts/${postId}`
+    );
+    const post = await response.json();
+    return post;
+}
+
+const page = async ({params}) => {
+	const post = await getSinglePost(params.postId);
+    if (!post) {
+        return <div>Loading...</div>;
+    }
+    return (
+        <div className="single-blog-page">
+            <h2>{post.title.rendered}</h2>
+            <div className="blog-post">
+                <p dangerouslySetInnerHTML={{ __html: post.content.rendered }}></p>
+            </div>
+        </div>
+    );
 };
 
 export default page;
--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,28 +1,32 @@
 import Link from 'next/link';
 
+// use fetch API to make a GET request to the /posts endpoint of the wp REST API
+async function getPosts() {
+    const response = await fetch(
+        `${process.env.NEXT_PUBLIC_WORDPRESS_API_URL}/posts`
+    );
+    const posts = await response.json();
+    return posts;
+}
+
 const BlogPage = async () => {
-	return (
-		<div className="blog-page">
-			<h2>All Blog Posts</h2>
-			<p>All blog posts are fetched from WordPress via the WP REST API.</p>
-			<div className="posts">
-				<Link href="/blog/1" className="post">
-					<h3>Hello World</h3>
-					<p>
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae
-						molestiae ducimus!
-					</p>
-				</Link>
-				<Link href="/blog/2" className="post">
-					<h3>Hello World</h3>
-					<p>
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae
-						molestiae ducimus!
-					</p>
-				</Link>
-			</div>
-		</div>
-	);
+    const posts = await getPosts();
+    return (
+        <div className="blog-page">
+            <h2>All Blog Posts</h2>
+            <p>All blog posts are fetched from WordPress via the WP REST API.</p>
+            <div className="posts">
+                {posts.map((post) => {
+                    return (
+                        <Link href={`/blog/${post.id}`} className="post" key={post.id}>
+                            <h3>{post.title.rendered}</h3>
+                            <p dangerouslySetInnerHTML={{ __html: post.excerpt.rendered }}></p>
+                        </Link>
+                    );
+                })}
+            </div>
+        </div>
+    );
 };
 
 export default BlogPage;
